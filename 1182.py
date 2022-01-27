cdf0c41e8f90833d536e6c97cffca491e346cdd5
tpark49 <typ5120@gmail.com>
2022-01-27 16:01:15 -0500

    problems

--- 1182.py
+++ 1182.py
@@ -1,36 +1,58 @@
+from cmath import inf
+
+
 def shortestDistanceColor(colors, queries):
-
-    result = []
-
-    #create dicitonary
-    index_dict = {}
-    for i, c in enumerate(colors): 
-        if c in index_dict: 
-            index_dict[c].append(i) 
+    color_map = {}
+    for index, color in enumerate(colors):
+        if color in color_map: 
+            color_map[color].append(index)
         else: 
-            index_dict[c] = [i] 
+            color_map[color] = [index]
 
-    print(index_dict)
 
-    for query in queries:
-        if query[1] in index_dict:
-            #check if there is any after last index, if not just return first 
-            if query[0]>index_dict[query[1]][-1]:
-                result.append(abs(index_dict[query[1]][-1]- query[0]))
-                continue
-            #if it is smaller than biggest index in dictionary... 
-            else:
-                #start with the smallest index in dictionary
-                i = 0
-                while i < len(index_dict[query[1]]):
-                    if index_dict[query[1]][i]>=query[0]:
-                        result.append(abs(index_dict[query[1]][i]-query[0]))
-                        break
-                    i+=1
-        #there is no number in dictionary so add -1 to list
-        else:
-            result.append(-1)
+    def binary_search(color, target):
+        if color not in color_map: 
+            return -1
+        color_index = color_map[color]
+
+        l = 0 
+        r = len(color_index)-1 
+
+        while l<=r:
+            mid = (l+r)//2
+
+            if color_index[mid] == target: 
+                return 0 
+
+            elif color_index[mid]>target:
+                r = mid - 1
+
+            else: 
+                l = mid + 1
+
+        left_index, right_index = float("inf"), float("inf")
+
+        if r>=0: 
+            right_index = abs(color_index[r] - target)
+
+        if l<len(color_index):
+            left_index = abs(color_index[l] - target)
+        
+        return min(right_index, left_index, abs(color_index[mid]-target))
+
+    result = [0] *len(queries)
+    for i, (query_index, query_color) in enumerate(queries):
+        if colors[query_index] != query_color:
+            shortest_distance = binary_search(query_color, query_index)
+            
+            if shortest_distance == float("inf"):
+                shortest_distance = -1
+
+            result[i] = shortest_distance
+
     return result
 
+
 if __name__ == "__main__":
+
     print(shortestDistanceColor([1,2],[[0,3]]))
