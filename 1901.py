007769a1fded01fe4afc333f714420a213ea06d7
tpark49 <typ5120@gmail.com>
2022-02-06 18:22:54 -0500

    problems

--- 1901.py
+++ 1901.py
@@ -1,41 +1,36 @@
 def findPeakGrid(mat):
-    
-    for row in range(len(mat)): 
-        if row == 0:
-            if mat[row][0]>mat[row][1] and mat[row][0] > mat[row+1][0]:
-                return [row, 0]
 
-            if mat[row][-1]>mat[row][-2] and mat[row][-1] > mat[row+1][-1]:
-                return [row, len(mat[0])-1]
+    start = 0 
+    end = len(mat[0])-1
+
+    while start <= end:
+
+        mid = (start + end)//2
+
+        largest_i, res = 0, float("-inf")
+        for i in range(len(mat)):
+            if mat[i][mid] > res: 
+                res = mat[i][mid]
+                largest_i = i 
         
-        if row == len(mat)-1:
-            if mat[row][0]>mat[row][1] and mat[row][0] > mat[row-1][0]:
-                return [row, 0]
+        left, right = False, False
+        if start>0 and mat[largest_i][mid]<mat[largest_i][mid-1]:
+            left = True 
 
-            if mat[row][-1]>mat[row][-2] and mat[row][-1] > mat[row-1][-1]:
-                return [row, len(mat[0])-1]
+        if end < len(mat[0])-1 and mat[largest_i][mid]<mat[largest_i][mid+1]:
+            right = True
 
-        l = 1 
-        r = len(mat[0])-2
+        if not left and not right: 
+            return [largest_i, mid] 
 
-        while l <= r: 
-            mid = (l+r)//2
+        if left: 
+            end = mid - 1
 
-            if row == 0:
-                if mat[row][mid]>mat[row][mid-1] and mat[row][mid]>mat[row][mid+1] and mat[row][mid]> mat[row+1][mid]:
-                    return [row, mid]
-            elif row == len(mat)-1:
-                if mat[row][mid]>mat[row][mid-1] and mat[row][mid]>mat[row][mid+1] and mat[row][mid] > mat[row-1][mid]:
-                    return [row, mid]
-            else: 
-                if mat[row][mid]>mat[row][mid-1] and mat[row][mid]>mat[row][mid+1] and mat[row][mid]>mat[row+1][mid] and mat[row][mid]>mat[row-1][mid]:
-                    return [row, mid]
+        else: 
+            start = mid + 1  
 
-            if mat[row][mid+1] > mat[row][mid]:
-                l = mid + 1
-            
-            else: 
-                r = mid - 1
+         
+
 
 if __name__ == "__main__":
     print(findPeakGrid([
