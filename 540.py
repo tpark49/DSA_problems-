0cdeb8d99016a9be07826eee82b71896e8bf0e9c
tpark49 <typ5120@gmail.com>
2022-02-13 23:14:04 -0500

    problems

--- 540.py
+++ 540.py
@@ -1,3 +1,21 @@
 def singleNonDuplicates(nums):
 
-    pass
+    l = 0
+    r = len(nums)-1
+
+    while l < r: 
+        mid = int((l + r)/2)
+        if (mid %2 == 0 and nums[mid]==nums[mid+1]) or (mid %2 ==1 and nums[mid] == nums[mid-1]):
+            l = mid + 1
+
+        else: 
+            r = mid 
+        
+    return nums[l] 
+
+if __name__ == "__main__":
+    print(singleNonDuplicates([1,1, 2,2,3,3,4,4,8]))
+
+
+        
+
